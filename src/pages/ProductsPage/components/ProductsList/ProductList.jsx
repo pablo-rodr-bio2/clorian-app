a076ff07c79f6fe0334904cef5e490fbe1da775c
pablo-rodr-bio2 <pablo.rodriguez@toptal.com>
2024-11-25 12:42:15 +0100

    Added sorting by text

--- src/pages/ProductsPage/components/ProductsList/ProductList.jsx
+++ src/pages/ProductsPage/components/ProductsList/ProductList.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { Box, InputLabel, MenuItem, Select } from '@mui/material'
+import { Box, Button, InputLabel, MenuItem, Select, TextField } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
@@ -7,32 +7,56 @@ const orderProducts = (products, order) => {
   return products.sort((a, b) => a[order].localeCompare(b[order]))
 }
 
+const sortProducts = (products, sort) => {
+  if(!sort) {
+    return products
+  }
+
+  return products.filter(product => product.name.toLowerCase().includes(sort.toLowerCase())
+    || product.description.toLowerCase().includes(sort.toLowerCase()))
+}
+
 const ProductList = ({ products }) => {
   const [ order, setOrder ] = useState('name')
-  const [ orderedProducts, setOrderedProducts ] = useState(orderProducts(products, order))
+  const [ sort, setSort ] = useState(undefined)
+  const [ filteredProducts, setFilteredProducts ] = useState([])
+
+  useEffect(() => {
+    const sortedProducts = orderProducts(products, order)
+    const orderedProducts = sortProducts(sortedProducts, sort)
+    setFilteredProducts(orderedProducts)
+  }, [products, order, sort])
 
   const handleOrder = (event) => {
     setOrder(event.target.value)
   }
 
-  useEffect(() => {
-    setOrderedProducts(previousOrderedProducts => orderProducts(previousOrderedProducts, order))
-  }, [order, orderedProducts])
+  const handleSort = (event) => {
+    setSort(event.target.value)
+  }
+
+  
 
   return (
     <>
-      <InputLabel id="order-by-label">Order by</InputLabel>
-      <Select
-        value={order}
-        onChange={handleOrder}
-        label="Order by"
-      >
-        <MenuItem value="name">Name</MenuItem>
-        <MenuItem value="description">Description</MenuItem>
-      </Select>
+      <Box>
+        <InputLabel id="order-by-label">Order by</InputLabel>
+        <Select
+          value={order}
+          onChange={handleOrder}
+          label="Order by"
+        >
+          <MenuItem value="name">Name</MenuItem>
+          <MenuItem value="description">Description</MenuItem>
+        </Select>
+
+        <InputLabel id="search">Search</InputLabel>
+        <TextField id="search" variant="outlined" value={sort} onChange={handleSort} />
+        <Button variant="contained" color="primary"  onClick={() => setSort('')}>Reset</Button>
+      </Box>
         
       <Box>
-          {orderedProducts.map(product => (
+          {filteredProducts.map(product => (
             <Box key={product.id}>
               <Link to={`/product/${product.id}`}>{product.name}</Link>
               <Box>{product.description}</Box>
